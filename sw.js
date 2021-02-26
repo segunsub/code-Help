98544ecb48add039d8949a5d9e1a757f45165034
segunsub <segunsubair3@gmail.com>
2021-02-26 14:50:13 -0500

    update to service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 self.addEventListener("install", e => {
     e.waitUntil( 
         caches.open("static").then(cache => {
-           return cache.addAll(["./", "./src","./icon"]) 
+            console.log(cache)
+           return cache.addAll(["index.html", "sw.js","manifest.json","src/index.js","src/index.css","src/prism.css","src/prism.js","src/align-justify.svg","icon/gold.png","icon/wrk.png","icon/xxxhdpi.png"]) 
         })
-    )    
-})    
-
+    )     
+})       
+ 
 self.addEventListener("fetch", e => {
    e.respondWith(
        caches.match(e.request).then(response => {
